128505ad640adaca23539fc423399317e849274f
Jaider Martinez <smithsaemirto45@gmail.com>
2023-04-25 08:50:52 -0500

    first commit

---
+++ src/main/java/com/sena/terminal/ChoferApplication.java
@@ -0,0 +1,13 @@
+package com.sena.terminal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChoferApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChoferApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sena/terminal/ChoferApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sena.terminal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChoferApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
